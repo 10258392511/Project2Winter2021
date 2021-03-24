ec7c97c4d9e3375fa736877ab5541a6be9ede0f1
Zhexin Wu <1025839251@qq.com>
2021-03-24 12:44:48 +0800

    part 1-3

--- proj2_nps.py
+++ proj2_nps.py
@@ -1,6 +1,6 @@
 #################################
-##### Name:
-##### Uniqname:
+##### Name: Zhexin Wu
+##### Uniqname: zhexinwu
 #################################
 
 from bs4 import BeautifulSoup
@@ -30,7 +30,27 @@ class NationalSite:
     phone: string
         the phone of a national site (e.g. '(616) 319-7906', '307-344-7381')
     '''
-    pass
+    def __init__(self, category, name, address, zipcode, phone):
+        self.category = category
+        self.name = name
+        self.address = address
+        self.zipcode = zipcode
+        self.phone = phone
+
+    def info(self):
+        """
+        Provides a string representation of the object.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        str
+            <name> (<category>): <address> <zip>
+        """
+        return f"{self.name} ({self.category}): {self.address} {self.zipcode}"
 
 
 def build_state_url_dict():
@@ -46,8 +66,28 @@ def build_state_url_dict():
         key is a state name and value is the url
         e.g. {'michigan':'https://www.nps.gov/state/mi/index.htm', ...}
     '''
-    pass
-       
+    out_dict = {}
+    base_url = "https://www.nps.gov"
+    main_url = "/index.htm"
+    unique_key = base_url + main_url
+    if unique_key in CACHE_DICT:
+        print("Using cache")
+        return CACHE_DICT[unique_key]
+
+    print("Fetching")
+    resp = requests.get(base_url + main_url)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    li_tags = soup.find("ul", class_="dropdown-menu SearchBar-keywordSearch").find_all("li")
+    for li_tag in li_tags:
+        anchor = li_tag.find("a")
+        ref = base_url + anchor["href"]
+        out_dict[anchor.text.lower()] = ref
+
+    CACHE_DICT[unique_key] = out_dict
+    save_cache(CACHE_DICT, CACHE_FILENAME)
+
+    return out_dict
+
 
 def get_site_instance(site_url):
     '''Make an instances from a national site URL.
@@ -62,7 +102,42 @@ def get_site_instance(site_url):
     instance
         a national site instance
     '''
-    pass
+    if site_url in CACHE_DICT:
+        print("Using cache")
+        json_dict = CACHE_DICT[site_url]
+        return NationalSite(**json_dict)
+
+    print("Fetching")
+    resp = requests.get(site_url)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    # print(soup)
+    try:
+        name = soup.find("a", class_="Hero-title").text.strip()
+    except Exception:
+        name = "No Name"
+    try:
+        cate = soup.find("span", class_="Hero-designation").text.strip()
+    except Exception:
+        cate = "No Category"
+    try:
+        zip_code = soup.find("span", attrs={"itemprop": "postalCode", "class": "postal-code"}).text.strip()
+    except Exception:
+        zip_code = "No Zipcode"
+    try:
+        phone = soup.find("span", attrs={"itemprop": "telephone", "class": "tel"}).text.strip()
+    except Exception:
+        phone = "No Phone"
+    try:
+        city = soup.find("span", attrs={"itemprop": "addressLocality"}).text.strip()
+        state = soup.find("span", attrs={"itemprop": "addressRegion", "class": "region"}).text.strip()
+        address = f"{city}, {state}"
+    except Exception:
+        address = "No Address"
+
+    CACHE_DICT[site_url] = {"category": cate, "name": name, "address": address, "zipcode": zip_code, "phone": phone}
+    save_cache(CACHE_DICT, CACHE_FILENAME)
+
+    return NationalSite(cate, name, address, zip_code, phone)
 
 
 def get_sites_for_state(state_url):
@@ -78,7 +153,27 @@ def get_sites_for_state(state_url):
     list
         a list of national site instances
     '''
-    pass
+    sites = []
+    if state_url in CACHE_DICT:
+        print("Using cache")
+        links = CACHE_DICT[state_url]
+
+    else:
+        print("Fetching")
+        base_url = "https://www.nps.gov"
+        resp = requests.get(state_url)
+        state_soup = BeautifulSoup(resp.text, "html.parser")
+        anchors = state_soup.select("ul li h3 a")
+        links = []
+        for anchor in anchors:
+            park_url = base_url + anchor["href"]
+            links.append(park_url)
+        CACHE_DICT[state_url] = links
+
+    for park_url in links:
+        sites.append(get_site_instance(park_url))
+
+    return sites
 
 
 def get_nearby_places(site_object):
@@ -95,7 +190,99 @@ def get_nearby_places(site_object):
         a converted API return from MapQuest API
     '''
     pass
-    
+
+
+def construct_unique_key(base_url, params, connector="_"):
+    """
+    Create a unique key for a query.
+
+    Parameters
+    ----------
+    base_url: str
+        API endpoint.
+    params: dict
+        Query parameters.
+    connector: str
+        Connecting symbol to string "base_url" and "params".
+
+    Returns
+    -------
+    str
+        The unique key as a str.
+    """
+    out_key = base_url
+    for key in params:
+        out_key =+ connector + f"{key}{connector}{params[key]}"
+
+    return out_key
+
+
+def open_cache(filename):
+    """
+    Open or create a cache.json file.
+
+    Parameters
+    ----------
+    filename: str
+        Path to the cache.json file.
+
+    Returns
+    -------
+    dict
+        Loaded or created cache as a dictionary.
+    """
+    try:
+        with open(filename, "r") as rf:
+            cache_dict = json.loads(rf.read())
+        # print("Using cache")
+        return cache_dict
+    except FileNotFoundError:
+        with open(filename, "w") as wf:
+            pass
+        # print("Fetching")
+        return {}
+
+
+def save_cache(cache_dict, filename):
+    """
+    Save the current cache dict to "filename".
+
+    Parameters
+    ----------
+    cache_dict: dict
+        The current cache dict.
+    filename: str
+        Cache file path.
+
+    Returns
+    -------
+    None
+    """
+    with open(filename, "w") as wf:
+        wf.write(json.dumps(cache_dict))
+
 
 if __name__ == "__main__":
-    pass
+    # # a test for missing info
+    # park_url = "https://www.nps.gov/yose/index.htm"
+    # site = get_site_instance(park_url)
+    # print(site.info())
+
+    # part 3
+    CACHE_FILENAME = "cache.json"
+    CACHE_DICT = open_cache(CACHE_FILENAME)
+    state_url_dict = build_state_url_dict()
+
+    while True:
+        user_input = input("Enter a state name (e.g. Michigan, michigan) or \"exit\": ")
+        if user_input == "exit":
+            break
+
+        state_url = state_url_dict[user_input.lower()]
+        title = f"List of national sites in {user_input}"
+        sites = get_sites_for_state(state_url)
+        print("-" * len(title))
+        print(title)
+        print("-" * len(title))
+        for i, site in enumerate(sites):
+            print(f"[{i + 1}] {site.info()}")
